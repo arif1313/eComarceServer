275a8ea3ca59ec52d3f83d5b4b9c166410af2aa0
arif1313 <arifhossen13131@gmail.com>
2024-05-22 23:55:37 +0600

    complet project

--- dist/app.js
+++ dist/app.js
@@ -12,6 +12,5 @@ exports.app.use(express_1.default.json());
 exports.app.use((0, cors_1.default)());
 exports.app.use('/ecomarce/v1/api', Product_routes_1.ProductRout);
 exports.app.get('/', (req, res) => {
-    const a = 2;
-    res.send(a);
+    res.send('Hello ');
 });
