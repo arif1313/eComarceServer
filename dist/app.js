2983cc8095c11b557afdc35967794ec6f61c0e1f
arif1313 <arifhossen13131@gmail.com>
2024-06-09 20:47:48 +0600

    api okey

--- dist/app.js
+++ dist/app.js
@@ -11,12 +11,12 @@ exports.app = (0, express_1.default)();
 exports.app.use(express_1.default.json());
 exports.app.use((0, cors_1.default)());
 exports.app.use('/api', Product_routes_1.ProductRout);
-// app.use((req: Request, res: Response) => {
-//   res.status(404).json({
-//     success: false,
-//     message: 'Route not found',
-//   });
-// });
 exports.app.get('/', (req, res) => {
     res.send({ message: 'hello   this is my ecomarce server ' });
+});
+exports.app.use((req, res) => {
+    res.status(404).json({
+        success: false,
+        message: 'Route not found',
+    });
 });
--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,13 @@ export const app: Application = express();
 app.use(express.json());
 app.use(cors());
 app.use('/api', ProductRout);
-// app.use((req: Request, res: Response) => {
-//   res.status(404).json({
-//     success: false,
-//     message: 'Route not found',
-//   });
-// });
 app.get('/', (req: Request, res: Response) => {
   res.send({ message: 'hello   this is my ecomarce server ' });
+});
+
+app.use((req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: 'Route not found',
+  });
 });
